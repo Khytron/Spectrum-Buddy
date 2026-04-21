e8c603fbbaf9a0e5bb2fbac3b8d453dd1108b0ae
M. Ikhmal Hakimie <24001471@siswa.um.edu.my>
2026-04-21 20:29:37 +0800

    Removed offscreen permission

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: 'src/popup/index.html',
-        offscreen: 'src/offscreen/offscreen.html',
         reminder: 'src/reminder/index.html',
       },
     },
